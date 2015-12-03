0ada5870a3d86c115b58fc99df6938e36370a087
Felipe <felipe.sacola@gmail.com>
2015-12-03 20:43:37 -0200

    Initial project creation

---
+++ desktop/src/io/github/djarroba/zombiegame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.djarroba.zombiegame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.djarroba.zombiegame.ZombieGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZombieGame(), config);
+	}
+}
